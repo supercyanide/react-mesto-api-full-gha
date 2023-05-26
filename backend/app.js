ddb7fcd97d7e925633df085be2af1fbe2d19bcf0
Lilya Schedrina <13cutepie@gmail.com>
2023-05-26 20:14:13 +0400

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -22,27 +22,29 @@ const { PORT = 3000 } = process.env;
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 const app = express();
-app.use(express.json());
-app.use(helmet());
-app.use(cookieParser());
-
-app.use(cors({ origin: '*' })); // ['http://localhost:3001', 'http://localhost:3001', 'https://supercyanide.nomoredomains.rocks', 'http://supercyanide.nomoredomains.rocks'], credentials: false, maxAge: 60 }));
-
-app.use(requestLogger);
-app.use(crashTestRouter);
-app.use(signInRouter);
-app.use(signUpRouter);
-
-app.use(auth);
-
-app.use(userRouter);
-app.use(cardRouter);
-app.use(errorLogger);
 
 app.use('*', (req, res, next) => {
   next(new NotFoundError('URL не найден'));
 });
-app.use(errors());
 
-app.use(errorHandler);
+// app.use(express.json());
+// app.use(helmet());
+// app.use(cookieParser());
+
+// app.use(cors({ origin: '*' })); // ['http://localhost:3001', 'http://localhost:3001', 'https://supercyanide.nomoredomains.rocks', 'http://supercyanide.nomoredomains.rocks'], credentials: false, maxAge: 60 }));
+
+// app.use(requestLogger);
+// app.use(crashTestRouter);
+// app.use(signInRouter);
+// app.use(signUpRouter);
+
+// app.use(auth);
+
+// app.use(userRouter);
+// app.use(cardRouter);
+// app.use(errorLogger);
+
+// app.use(errors());
+
+// app.use(errorHandler);
 app.listen(PORT);
