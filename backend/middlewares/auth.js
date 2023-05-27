1ab3d5b04045f8efc84c6e4e63ce31d13786b52f
Lilya Schedrina <13cutepie@gmail.com>
2023-05-27 21:08:13 +0400

    Update auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,13 +2,16 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
-const UnauthorizedError = require('../errors/UnauthorizedError');
+// const UnauthorizedError = require('../errors/UnauthorizedError');
 
+// eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const token = req.headers.authorization.replace('Bearer ', '');
 
   if (!token) {
-    throw new UnauthorizedError('Вы не авторизованы');
+    return res
+      .status(401)
+      .send({ message: 'Необходима авторизация' });
   }
 
   let payload;
@@ -16,7 +19,9 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-key');
   } catch (err) {
-    next(UnauthorizedError(err.message));
+    return res
+      .status(401)
+      .send({ message: 'Необходима авторизация' });
   }
 
   req.user = payload;
